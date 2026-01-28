3fb94bff213da85ee685948afacc7a0a0305abda
BioSh0cked <17990284+BioSh0cked@users.noreply.github.com>
2026-01-28 06:15:44 -0500

    First heartbeat success

--- tdwakatime.py
+++ tdwakatime.py
@@ -4,6 +4,7 @@ import time
 import subprocess
 import platform
 import json
+import re
 
 try:
 	from urllib2 import Request, urlopen, HTTPError
@@ -11,28 +12,25 @@ except ImportError:
 	from urllib.request import Request, urlopen
 	from urllib.error import HTTPError
 
+
 GITHUB_PREFIX = 'https://github.com/wakatime/wakatime-cli/releases/download'
 GITHUB_LATEST_URL = 'https://api.github.com/repos/wakatime/wakatime-cli/releases/latest'
 is_win = platform.system() == 'Windows'
 
-class Wakatime:
-	def __init__(self, ownerComp):
+class WakaExt:
+	def __init__(self, ownerComp) -> None:
 		self.owner = ownerComp
 		self.last_cook = 0
 		self.last_heartbeat = 0
 		self.idle_timeout = 60
-		self.api_key = self.get_key()
-		self.cli_path = self.get_cli()
-		if not hasattr(project, 'Plugin_Initialized'):
-			project.addCallback(self.Event)
-			project.Plugin_initialized = True
-		print("Wakatime plugin initialized")
-
-	def Event(self, event):
-		if event['name'] in ['onValueChange', 'onCreate', 'onProjectPostSave']:
-			self.last_cook = time.time()
-			self.send_heartbeat(event)
-	def get_latest_client_version():
+		self.api_key = self.Get_key()
+		self.cli_path = self.Get_cli()
+	@property
+	def Event(self, entity):
+		self.last_cook = time.time()
+		self.Send_heartbeat(entity)
+		return
+	def Get_latest_client_version(self):
 		latest_version = "0"
 		config = os.path.expanduser("~/.wakatime/wakatime-internal.cfg")
 		if config:
@@ -53,15 +51,16 @@ class Wakatime:
 					osname=platform.system().lower(),
 					arch=platform.machine() or platform.processor(),
 				))
-	def get_key():
-		global API_KEY
+	def Get_key(self):
 		config = os.path.expanduser("~/.wakatime.cfg")
 		if os.path.exists(config):
 			with open(config, "r") as f:
 					for line in f:
+						if line in f.startswith('api_url'):
+							api_url = line.split('=')[1].strip()
 						if line.startswith("api_key"):
 								return line.split("=")[1].strip()
-	def get_cli():
+	def Get_cli(self):
 		global WAKATIME_CLI_PATH
 
 		WAKATIME_CLI_PATH = os.path.join("~/.wakatime/wakatime-cli")
@@ -75,21 +74,16 @@ class Wakatime:
 			WAKATIME_CLI_PATH = os.path.join("~/.wakatime/", waka_cli)
 		return WAKATIME_CLI_PATH
 
-	API_KEY = get_key()
-	WAKATIME_CLI_PATH = get_cli()
-
-	def heartbeat(self, entity=None):
-		if not API_KEY or not WAKATIME_CLI_PATH:
+	def Send_heartbeat(self, entity):
+		if not self.api_key or not WAKATIME_CLI_PATH:
 			return
 
 		if time.time() - self.last_heartbeat < 60:
 			return
-		entity = project.path
-		project_name = os.path.basename(entity)
-
+		project_name = re.sub(r'\.\d+(?=\.toe$)', '', project.name)
 		try:
 			subprocess.Popen([
-				self.cli_path,
+				os.path.expanduser(self.cli_path),
 				'--key', self.api_key,
 				'--entity', entity,
 				'--project', project_name,
